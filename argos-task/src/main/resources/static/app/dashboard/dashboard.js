d73acf22c12c309e26950deb2259e27989f4faab
Ashley <ashmuswere@gmail.com>
2016-02-07 23:36:57 +0000

    Fixed bug

--- argos-task/src/main/resources/static/app/dashboard/dashboard.js
+++ argos-task/src/main/resources/static/app/dashboard/dashboard.js
@@ -21,7 +21,7 @@ angular.module('myApp.dashboard', [
             $http.get('http://localhost:8080/api/products')
                 .success(function(data, status, headers, config) {
                     data.sort(function (a, b) {
-                        return parseFloat(parseFloat(b.priceDifference) - (a.priceDifference))
+                        return parseFloat(parseFloat(b.temperature) - (a.temperature))
                     });
                     $scope.data = data.slice(0,10);
                     console.log($scope.data)
